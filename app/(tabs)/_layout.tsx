1b887ee03be66fadf41d52acc8c444c65c97cc27
filipvlkous <thiefcz@email.cz>
2025-12-02 00:46:36 +0100

    change tabs UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,22 @@
-import { Tabs } from 'expo-router';
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 import { TouchableOpacity, Text } from 'react-native';
 import { useAuthStore } from '../../store/authStore';
 
 export default function TabLayout() {
-  const { signOut } = useAuthStore();
-
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: true,
-        tabBarActiveTintColor: '#2563eb',
-        headerRight: () => (
-          <TouchableOpacity onPress={signOut} className="mr-4">
-            <Text className="font-medium text-blue-600">Sign Out</Text>
-          </TouchableOpacity>
-        ),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Activities',
-          tabBarLabel: 'Activities',
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="upload"
-        options={{
-          title: 'Upload',
-          tabBarLabel: 'Upload',
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarLabel: 'Profile',
-          headerShown: false,
-        }}
-      />
-    </Tabs>
+    <NativeTabs minimizeBehavior="onScrollDown">
+      <NativeTabs.Trigger name="index">
+        <Label>Activities</Label>
+        <Icon sf={'house'} />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="upload">
+        <Label>Upload</Label>
+        <Icon sf={'paperplane.fill'} />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="profile">
+        <Label>Profile</Label>
+        <Icon sf={'person.fill'} />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
